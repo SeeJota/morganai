3d4c5ab52e3204859296ecae0c19ecb949c5514a
Khoverex <luis.luismateo@outlook.com>
2023-10-07 14:24:47 -0400

    start of saturday

---
+++ src/ai/aiTraining.java
@@ -0,0 +1,41 @@
+package src.ai;
+
+import java.util.*;
+
+public class aiTraining{
+    public static void main(String[] args){
+        System.out.println("Initializing test");
+
+
+
+        //set up basic net work
+
+        ArrayList<neuron> inputNurons = new ArrayList<>(Arrays.asList(new neuron(),
+                                                                        new neuron()));
+        //initializing values
+        foreach(neuron n : inputNurons){
+            n.value = 1f;
+            //n.initialize();
+        }
+
+        ArrayList<neuron> hiddenNuronLayer1 = new ArrayList<>(Arrays.asList(new neuron(0f,inputNurons, new ArrayList<>(.2f,.4f)),
+                                                                        new neuron(0f,inputNurons, new ArrayList<>(.2f,.4f)),
+                                                                        new neuron(0f,inputNurons, new ArrayList<>(.2f,.4f))));
+
+        ArrayList<neuron> outputNurons = new ArrayList<>(Arrays.asList(new neuron(0f,hiddenNuronLayer1, new ArrayList<>(.2f,.4f,.3f)),
+                                                                        new neuron(0f,hiddenNuronLayer1, new ArrayList<>(.2f,.4f,.3f)));
+        //computes hidden nurons
+        foreach(neuron n : hiddenNuronLayer1){
+            n.compute();
+
+        }
+        //computes output nurons
+        foreach(neuron n : outputNurons){
+            n.compute();
+            System.out.println("nuron output = " + n.value);
+        }
+
+
+
+    }
+}
---
+++ src/ai/neuron.java
@@ -0,0 +1,41 @@
+package src.ai;
+import java.util.ArrayList;
+import java.lang.Math;
+public class neuron{
+
+    private float bias = 0.0f;
+    public float value = 0.0f;
+    private ArrayList<neuron> inputNurons;
+    private ArrayList<Float> weightedInputOfInputNurons;
+
+    public neuron(float bias, ArrayList<neuron> inputNurons, ArrayList<Float> weightedInputOfInputNurons){
+        this.bias = bias;
+        this.inputNurons = inputNurons;
+        this.weightedInputOfInputNurons = weightedInputOfInputNurons;
+    }
+    public neuron(float bias){
+        this.bias = bias;
+    }
+    public neuron(){
+        this.bias = 0.0f;
+    }
+    public void compute(){
+
+        float sum =0.0f;
+        //loops through and sums all pairings of weights to nurons
+        for(int i =0; i < inputNurons.size() && i < weightedInputOfInputNurons.size(); i++){
+            sum = sum + inputNurons.get(i).getValue() * weightedInputOfInputNurons.get(i);
+        }
+        //applies bias and sigmoid
+        value = aiFunctions.getSigmoid(sum+ bias);
+    }
+    public void initialize(){
+        value = (float)(Math.random());
+    }
+
+    public float getValue(){
+        return value;
+    }
+
+}
+
