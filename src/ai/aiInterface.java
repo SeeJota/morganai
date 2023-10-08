9cbccdd8c7bff9dcb93cb5f916b92c80dfb3b76f
Carlos Rodriguez <cjeremy33196@gmail.com>
2023-10-07 21:03:55 -0400

    Altering the read and write functions.

--- src/ai/aiInterface.java
+++ src/ai/aiInterface.java
@@ -1,7 +1,55 @@
 package src.ai;
+
+import java.io.*;
+import java.util.ArrayList;
+
 public class aiInterface{
+    static final String FILE_PATH = "/data/neuron_info.csv";
     public static void main(String[] args){
         System.out.println("hellowowofwfoerhgerdoifngbvodfkbnhdofbnhdfobghnfd");
 
+
+
+
     }
+    private ArrayList<ArrayList<neuron>> getNurons(){
+        return aiTraining.networkNeurons;
+    }
+    public void writeNeuronToFile() throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(FILE_PATH); //"/data/neuron_info.csv"
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            try {
+                fos.getChannel().truncate(0);
+            } catch (IOException e) {
+            }
+            oos.writeObject(getNurons());
+//            for (ArrayList<neuron> aN : getNurons()) {
+//                oos.writeObject(aN);
+////
+////                for(neuron n : aN){
+////                    oosf.writeObject(n);
+////                }
+//            }
+            oos.flush();
+        } catch( IOException e){
+
+        }
+
+    }
+    public ArrayList<ArrayList<neuron>> readObjFromFile() throws IOException, ClassNotFoundException{
+        ArrayList<ArrayList<neuron>> returnal = null;
+        try (FileInputStream fis = new FileInputStream(FILE_PATH); //"/data/neuron_info.csv"
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+
+            returnal = (ArrayList<ArrayList<neuron>>) ois.readObject();
+        }catch( IOException e) {
+        }
+
+        return returnal;
+
+
+    }
+
+
+
 }
